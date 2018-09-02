bd715a11488ca32aa23a5397dde999d0e68b652c
Harald Hartmann <osm@haraldhartmann.de>
2018-09-02 12:43:14 +0200

    don't add usergroups without name to lastgroups.cache

--- usergroups.py
+++ usergroups.py
@@ -13,48 +13,55 @@ import os.path
 import sys
 import logging
 
+
 def saveCache(groups):
-    fcache=file("usergroups.cache","w")
-    pickle.dump(groups,fcache)
+    fcache = file("usergroups.cache", "w")
+    pickle.dump(groups, fcache)
     fcache.close()
 
+
 def loadCache():
-    groups={}
+    groups = {}
     try:
-        fcache=file("usergroups.cache","r")
-        groups=pickle.load(fcache)
-        fcache.close()        
+        fcache = file("usergroups.cache", "r")
+        groups = pickle.load(fcache)
+        fcache.close()
     except IOError:
         logging.log(logging.WARNING, "no cache file present!")
-    return groups    
+    return groups
+
 
 def getNewestUserGroups(currGroups):
-    oldGroups=[]
-    newGroups=[]
-    #flatten lists
+    oldGroups = []
+    newGroups = []
+    # flatten lists
     for group in loadCache():
-        oldGroups.append(group["name"])
+        if group["name"] != "":
+            oldGroups.append(group["name"])
     for group in currGroups:
-        newGroups.append(group["name"])
-    diff = list(set(newGroups)-set(oldGroups))
-    diff=__appendNewestUserGroups(diff)
+        if group["name"] != "":
+            newGroups.append(group["name"])
+    diff = list(set(newGroups) - set(oldGroups))
+    diff = __appendNewestUserGroups(diff)
     return diff
+
 
 def __appendNewestUserGroups(newgroups):
     try:
-        fcache=file("lastgroups.cache","r")
-        lastgroups=pickle.load(fcache)
-        fcache.close()        
+        fcache = file("lastgroups.cache", "r")
+        lastgroups = pickle.load(fcache)
+        fcache.close()
     except IOError:
         logging.log(logging.WARNING, "no lastgroups file present!")
-        lastgroups=[]
-    lastgroups=newgroups+lastgroups
-    if len(lastgroups)>=5:
-        lastgroups=lastgroups[0:4]
-    fcache=file("lastgroups.cache","w")
-    pickle.dump(lastgroups,fcache)
+        lastgroups = []
+    lastgroups = newgroups + lastgroups
+    if len(lastgroups) >= 5:
+        lastgroups = lastgroups[0:4]
+    fcache = file("lastgroups.cache", "w")
+    pickle.dump(lastgroups, fcache)
     fcache.close()
     return lastgroups
+
 
 def _exportUserGroupsJSON(groups, filename, langCodes=None):
     features = []
@@ -66,47 +73,56 @@ def _exportUserGroupsJSON(groups, filename, langCodes=None):
             lat = float(ugroup["lonlat"][1])
             properties = ugroup.copy()
             properties.pop("lonlat", None)
-            features.append(geojson.Feature(geometry=geojson.Point((lon, lat)), properties=properties))
+            features.append(geojson.Feature(
+                geometry=geojson.Point((lon, lat)), properties=properties))
         except ValueError as err:
-            logging.log(logging.WARNING, "Coordinate parse error for user group {}: {}".format(ugroup.get("name", ""), err))
+            logging.log(logging.WARNING, "Coordinate parse error for user group {}: {}".format(
+                ugroup.get("name", ""), err))
     collection = geojson.FeatureCollection(features)
-    with codecs.open(filename,'w+',"utf-8") as f:
+    with codecs.open(filename, 'w+', "utf-8") as f:
         geojson.dump(collection, f)
+
 
 def exportUserGroupsJSON(groups, filename):
     _exportUserGroupsJSON(groups, filename)
 
+
 def exportUserGroupsCountriesJSON(groups, langCodes, filename):
     _exportUserGroupsJSON(groups, filename, langCodes)
 
-def exportUserGroups(groups,filename):
-    #create a KML
-    k=mykml.kml("OSM usergroups worldwide","Generated list of OpenStreetMap local user groups by UserGroupsBot")
-    k.add_style("usergroup","localgroup.png")   
-    #save groups
+
+def exportUserGroups(groups, filename):
+    # create a KML
+    k = mykml.kml("OSM usergroups worldwide",
+                  "Generated list of OpenStreetMap local user groups by UserGroupsBot")
+    k.add_style("usergroup", "localgroup.png")
+    # save groups
     for ugroup in groups:
-        point=ugroup["lonlat"]
-        name=ugroup["name"]
-        rest=ugroup.copy();
+        point = ugroup["lonlat"]
+        name = ugroup["name"]
+        rest = ugroup.copy()
         del rest["lonlat"]
-        del rest["name"]   
-        k.add_placemark(name,point,"usergroup",rest)
+        del rest["name"]
+        k.add_placemark(name, point, "usergroup", rest)
     k.save(filename)
 
-def exportUserGroupsCountries(groups,langCodes,filename):
-    countries=[]
+
+def exportUserGroupsCountries(groups, langCodes, filename):
+    countries = []
     for ugroup in groups:
-        if ugroup["country"] in langCodes: 
-                countries.append(ugroup)
-    k=mykml.kml("OSM usergroups","Generated list of OpenStreetMap local user groups by UserGroupsBot")
-    k.add_style("usergroup","localgroup.png")
+        if ugroup["country"] in langCodes:
+            countries.append(ugroup)
+    k = mykml.kml("OSM usergroups",
+                  "Generated list of OpenStreetMap local user groups by UserGroupsBot")
+    k.add_style("usergroup", "localgroup.png")
     try:
         for ugroup in countries:
-            point=ugroup["lonlat"]
-            name=ugroup["name"]
-            rest=ugroup.copy();
+            point = ugroup["lonlat"]
+            name = ugroup["name"]
+            rest = ugroup.copy()
             del rest["lonlat"]
             del rest["name"]
-            k.add_placemark(name,point,"usergroup",rest)            
+            k.add_placemark(name, point, "usergroup", rest)
         k.save(filename)
-    except KeyError: sys.stderr.write("\nBad usergroup in countries")   
+    except KeyError:
+        sys.stderr.write("\nBad usergroup in countries")
